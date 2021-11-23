b610abf3988ee2c99b5ec9652986dc95de65c62d
Steffen Kober <steffen.kober@tu-dortmund.de>
2021-11-23 22:26:42 +0100

    ersten plot angefangen wollts nur schonma pushen damit et keinen konflikt gibt falls du auch schon anfängst xD

--- v105/plot.py
+++ v105/plot.py
@@ -1,21 +1,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.optimize
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+import scipy.constants as const
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+r_m, I = np.genfromtxt("content/Messung1.txt",unpack = True)
+N = 195
+R_Spule = 0.109
+d = 0.138
+B = ((const.mu_0)*I*(R_Spule**2))/(((R_Spule**2)+(d/2)**2)**(3/2))
 
 plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+plt.plot(r_m, B, label='Bestimmung des magnetischen Moments einer Vollkugel durch Gleichgewichtisbetrachtung')
+plt.xlabel("$t\, / \, m$ ")
+plt.ylabel("$B\, / \, mT$")
+#plt.legend(loc='best')
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+#plt.savefig('build/plot.pdf')
+#plt.close()
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+#plt.subplot(1, 2, 2)
+#plt.plot(x, y, label='Kurve')
+#plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
+#plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
+#plt.legend(loc='best')
+
+#plt.savefig('build/plot.pdf')
+#plt.close()
