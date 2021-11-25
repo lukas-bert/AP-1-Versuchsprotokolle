7495be76d265323fdc395e1c834273cf3bba3cd7
Steffen Kober <steffen.kober@tu-dortmund.de>
2021-11-25 19:22:08 +0100

    kleine aenderung am plot

--- v105/plot.py
+++ v105/plot.py
@@ -8,10 +8,11 @@ import scipy.constants as const
 
 r_m, I = np.genfromtxt("content/Messung1.txt",unpack = True)
 I_1 = unp.uarray(I,0.05)
-r_m = unp.uarray(r_m,0.00001)
-r_z = ufloat(0.995/2, 0.00001)
-r_ks = ufloat(1.25, 0.00001)
-r_ges = (r_m +  r_z + r_ks) #in cm
+r_m = unp.uarray(r_m,0.001)
+r_z = ufloat(0.995/2, 0.001)
+r_ks = ufloat(1.25, 0.001)
+r_k = ufloat(5.4/2,0.001)
+r_ges = (r_m +  r_z + r_ks + r_k) #in cm
 N = 195
 R_Spule = 0.109
 d = 0.138
@@ -21,12 +22,12 @@ def f(x, m, b):
     return m*x + b
 parameters, pcov = op.curve_fit(f, unp.nominal_values(r_ges), unp.nominal_values(B), sigma=unp.std_devs(B))
 err = np.sqrt(np.diag(pcov))
-x = np.linspace(0,10,100)
+x = np.linspace(0,15,100)
 
 plt.subplot(1, 2, 1)
-plt.errorbar(unp.nominal_values(r_ges), unp.nominal_values(B),xerr = unp.std_devs(r_ges),yerr = unp.std_devs(B), fmt='ro',label='Messdaten')
+plt.errorbar(unp.nominal_values(r_ges), unp.nominal_values(B),xerr = unp.std_devs(r_ges),yerr = unp.std_devs(B), fmt='r.',label='Messdaten')
 plt.plot(x,f(x,parameters[0],parameters[1]), label='lineare Regression')
-plt.xlim(0, 8.5)
+plt.xlim(3, 12)
 plt.xlabel("$r\, / \, \mathrm{cm}$ ")
 plt.ylabel("$B\, / \, \mathrm{T}$")
 plt.legend(loc='best')
