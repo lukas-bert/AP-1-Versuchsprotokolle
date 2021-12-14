5ad367b4b41e8a4359e26a4883d5bf717c94c0b0
Lukas Bertsch <lukas.bertsch@tu-dortmund.de>
2021-12-14 15:34:04 +0100

    Hysterese Plot angefangen

--- v308/plot.py
+++ v308/plot.py
@@ -3,7 +3,6 @@ import numpy as np
 
 I, B = np.genfromtxt("content/dataHysterese.txt", unpack = True)
 
-plt.subplot(1, 2, 1)
 plt.plot(I, B, 'r.', label='Messwerte')
 plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
 plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
