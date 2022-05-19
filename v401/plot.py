a31dc79f483338cf8b861d5b9eb6bbb226e07963
Lukas Bertsch <lukas.bertsch@tu-dortmund.de>
2022-05-19 18:00:01 +0200

    Auswertung anfang

--- v401/plot.py
+++ v401/plot.py
@@ -1,21 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import uncertainties as unc
+from uncertainties import unumpy as unp 
+from uncertainties import ufloat
+from uncertainties.unumpy import nominal_values as noms
+from uncertainties.unumpy import std_devs as devs
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+n1 = np.genfromtxt("content/data/data1.txt", unpack = True)
+n2 = np.genfromtxt("content/data/data2.txt", unpack = True)
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+u = 1/5.046         # Untersetzungsverhältnis
+d = 5e-3            # m Verschiebung des Spiegels 
+laser = 635e-9      # m Wellenlänge des Lasers
+b = 50e-3           # m Schichtdicke der Messzelle
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+T_0 = 273.15        # K
+T   = 295.14        # K, Raumtemperatur (22°C)
+p_0 = 101325        # Pascal
+d_p =  60000        # Pascal Druch nach Vakkumpumpen
+
+z1 = ufloat(np.mean(n1), np.std(n1))
+print("Mittelwert der Zählraten: ", z1)
+
+laser_exp = 2*d*u/z1
+d_laser = np.abs(noms(laser_exp)- laser)/laser
+
+print("Experimentelle Wellenlänge: ", '{0:.5e}'.format(laser_exp))
+print("Abweíchung:                 ", d_laser)
+
+
