e43fa7048dafb2b227495dcea1a2fced73f0f5c3
Lukas Bertsch <lukas.bertsch@tu-dortmund.de>
2022-06-09 19:24:05 +0200

    Theorie denke ich fertig und plot.py auch, Auswertung angefangen

--- v703/plot.py
+++ v703/plot.py
@@ -1,21 +1,80 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import uncertainties as unc
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
+from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
+import scipy.constants as const
+import scipy.optimize as op
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+U, Z, I = np.genfromtxt("content/data/data1.txt", unpack = True)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+# Fehler der Messgroßen
+Z = unp.uarray(Z, np.sqrt(Z))
+N = Z/120                       # 120 Sekunden gemessen
+I = unp.uarray(I, 0.1)*10**(-6)
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+# Charakteristische Kurve des Zählrohres
+down, up = [3, 31]
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+# Linearer Fit
+def f(x, a, b):
+    return a*x + b
+
+params, pcov = op.curve_fit(f, U[down:up], noms(N[down:up]))
+
+# Parameter
+err = np.sqrt(np.diag(pcov))
+a = ufloat(params[0], err[0])
+b = ufloat(params[1], err[1])
+
+print("------------------------------------------------------------------------")
+print("Parameter des Fits")
+print("a:               ", a, "[1/V] = ", 100*a, "[1/100V]")
+print("b:               ", b, "[V]")
+print("Plateaulänge:    ", U[up-1] - U[down], "[V]")
+print("------------------------------------------------------------------------")
+
+# Plot
+
+x = np.linspace(U[down], U[up-1], 100)
+
+plt.errorbar(U, noms(N), yerr = stds(N), marker = ".", linestyle = None, color = "firebrick", capsize = 3, linewidth = 0, elinewidth = 1)
+plt.errorbar(U[down:up], noms(N[down:up]), yerr = stds(N[down:up]), marker = ".", linestyle = None, color = "forestgreen", capsize = 3,
+                linewidth = 0, elinewidth = 1, label = "Plateaubereich")
+plt.plot(x, f(x, *params), color = "cornflowerblue", label = "Regression")                
+
+plt.xlabel(r"$U \mathbin{/} \unit{\volt}$")
+plt.ylabel(r"$N \mathbin{/} \unit{\second^{-1}}$")
+
+plt.grid()
+plt.legend()
+#plt.show()
+
 plt.savefig('build/plot.pdf')
+plt.close()
+
+# Totzeit über 2 Quellen Methode
+
+N1, N12, N2 = np.genfromtxt("content/data/data2.txt", unpack = True)
+
+# Fehler der Messgrößen
+N1 = ufloat(N1, np.sqrt(N1))
+N2 = ufloat(N2, np.sqrt(N2))
+N12 = ufloat(N12, np.sqrt(N12))
+
+T = (N1 + N2 - N12)/(2*N1*N2)
+
+print("------------------------------------------------------------------------")
+print("Totzeit über 2-Quellen-Methode:", '{0:.3e}'.format(T))
+print("------------------------------------------------------------------------")
+
+# Pro Teilchen freiwerdende Ladung
+dt = 120 # s
+
+dQ = I*dt/(Z*const.e)
+print("------------------------------------------------------------------------")
+print("Ladungen in eV")
+for i in range(len(dQ)):
+    print('{0:.3e}'.format(dQ[i]), "eV")
+print("------------------------------------------------------------------------")
